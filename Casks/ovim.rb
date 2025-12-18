88847bd3827739536574cdfe2858615da7732d3a
tonisives <tonisives@gmail.com>
2025-12-18 07:01:06 +0700

    Update ovim repo URL

--- Casks/ovim.rb
+++ Casks/ovim.rb
@@ -7,10 +7,10 @@ cask "ovim" do
   version :latest
   sha256 arm: "cc7d24a178aead00353f606766a2c57e0d92866ed205626e590475a82c36f276", intel: "cdc3c267ac32181344a6fbcf2ae48a730bdb5388af74466748188448cbaa8f7d"
 
-  url "https://github.com/tonisives/ti-vim-rust/releases/latest/download/ovim_#{arch}.dmg"
+  url "https://github.com/tonisives/ovim/releases/latest/download/ovim_#{arch}.dmg"
   name "ovim"
   desc "System-wide Vim mode for macOS"
-  homepage "https://github.com/tonisives/ti-vim-rust"
+  homepage "https://github.com/tonisives/ovim"
 
   livecheck do
     url :url
